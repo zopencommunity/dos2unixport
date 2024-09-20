42ce97e753e94d67b6a16a519d5d29561cd045b8
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:07 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/dos2unixport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/dos2unixport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/dos2unixport.git'), string(name: 'PORT_DESCRIPTION', value: 'Text file format converters' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/dos2unixport.git'), string(name: 'PORT_DESCRIPTION', value: 'Text file format converters' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
